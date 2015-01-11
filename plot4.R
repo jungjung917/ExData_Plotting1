258068c866a6b5814cf4c9bce03bb4173778b63c
Lucia <zhengshizhao917@gmail.com>
2015-01-11 16:20:01 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@ colClasses = c("character","character","numeric","numeric","numeric"
 subdata <- subset(data1, Date == "1/2/2007" | Date == "2/2/2007")
 subdata$DateTime = as.POSIXct(strptime(paste(subdata$Date, subdata$Time), format="%d/%m/%Y %H:%M:%S"))
 
-png(filename = "Plot4.png", width = 480, height = 480)
+png(filename = "plot4.png", width = 480, height = 480)
 par(mfcol = c(2,2),mar=c(5,5,3,2))
 plot(subdata$DateTime, subdata$Global_active_power, type = 'n', ylab="Global Active Power",xlab = "")
 lines(subdata$DateTime, subdata$Global_active_power)
