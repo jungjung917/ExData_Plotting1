b70952ff9deee561dfaab4284f87eebd34d32047
Lucia <zhengshizhao917@gmail.com>
2015-01-11 16:19:12 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ subdata <- subset(data1, Date == "1/2/2007" | Date == "2/2/2007")
 subdata$DateTime = as.POSIXct(strptime(paste(subdata$Date, subdata$Time), format="%d/%m/%Y %H:%M:%S"))
 head(subdata)
 
-png(filename = "Plot2.png", width = 480, height = 480)
+png(filename = "plot2.png", width = 480, height = 480)
 par(mar=c(3,5,2,2))
 plot(subdata$DateTime, subdata$Global_active_power, type = 'n', ylab="Global Active Power (Kilowatts)")
 lines(subdata$DateTime, subdata$Global_active_power)
